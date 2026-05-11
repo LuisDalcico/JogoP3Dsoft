dca6e325bb4eb920a7b29156abad482dc6768156
LuisDalcico <luisdalcico@outlook.com>
2026-05-11 17:54:20 -0300

    V1

--- Game.py
+++ Game.py
@@ -1,1 +1,20 @@
 #arquivoPrincipal
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pássaro")
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            game = False
+
+    window.fill((255, 255, 255))
+    pygame.display.update()
+
+pygame.quit()
