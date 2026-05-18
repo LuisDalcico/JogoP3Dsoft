3668703d0a38a1c892418da9bc046f4990ace36d
xpatuepe <xapatuepe@gmail.com>
2026-05-17 22:17:47 -0300

    adição de obstáculo

--- Game.py
+++ Game.py
@@ -9,11 +9,17 @@ pygame.init()
 window = pygame.display.set_mode((800, 600)) #janela do jogo com largura de 800 e altura de 600
 Fundojogo = pygame.image.load("Fundojogo.png") #imagem do fundo do jogo 
 passaro = pygame.image.load("pngwing.com.png") #importando a imagem 
+pipeTop = pygame.image.load("NicePng_nes-png_756849.png")#imagem do topo do obstáculo
+pipeMiddle = pygame.image.load("NicePng_nes-png_75.png")#imagem da parte de baixo do obstáculo que pode ser usada para aumentar a altura dos tubos
 personagem = pygame.transform.scale(passaro, (80, 80)) #imagem redimensionada 
-personagemX=100 #posição x do personagem
-personagemY=100 #posição y do personagem
+personagemX = 100 #posição x do personagem
+personagemY = 100 #posição y do personagem
+obstáculo = pygame.transform.scale(pipeTop, (80,80))#redimensionando
+obstX = 800 #posição x do obstáculo
+obstY = 520 #posição y do obstáculo
 pygame.display.set_caption("Pássaro") #mostra o nome do jogo na barra da janela 
 relogio = pygame.time.Clock() #relogio para controlar a taxa de atualização do jogo
+obstSpeed = 5 #velocidade com a qual os obstáculos se moverão pela tela
 
 game = True # condição para o jogo funcionar 
 
@@ -27,8 +33,12 @@ while game:
             if event.key == pygame.K_SPACE:
                 personagemY-=30 #Sempre que a tecla espaço é pressionada, o passaro sobe 30 pixels
     personagemY+=1 #desce o passaro em 1 pixel a cada frame
+    obstX-=obstSpeed #o obstáculo se move em direção ao pássaro baseado no obstSpeed a cada frame
+    if obstX<-80:
+        obstX = 800#faz o obstáculo voltar ao início após sair da tela
     window.blit(Fundojogo, (0, 0)) #desenha o fundo da janela do jogo 
     window.blit(personagem, (personagemX, personagemY)) #desenha o personagem
+    window.blit(obstáculo, (obstX, obstY)) #desenha o obstáculo
     pygame.display.update()
 
 pygame.quit()
